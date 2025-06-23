a153bc642da5c54a7374d9924b3321da945fa086
MateoCortésD <57098389+MateoCorDuarte@users.noreply.github.com>
2025-06-23 20:35:32 +0200

    Update barber_shop.c

--- C/barber_shop.c
+++ C/barber_shop.c
@@ -10,12 +10,27 @@ In the case of a fire alarm, all customers must leave the shop in an orderly fas
 */
 #include "mbed.h"
 
-#define BUTTON_1 p5
-#define BUTTON_2 p6 
-#define SWITCH_1 p7 
-#define SWITCH_2 p8 
+#define BUTTON_1 p5 //ADULT
+#define BUTTON_2 p6 //child
+#define BUTTON_3 p7 //fire alarm
 
-#define RED_LED p9
-#define BLUE_LED p10
-#define YELLOW_LED p11
-#define WHITE_LED p12
+#define RED_LED p8 //No entry light
+#define BLUE_LED p9 //barber 1 seat
+#define YELLOW_LED p10 //barber2 seat
+#define WHITE_LED p11 //barber 3 seat
+#define WHITE_LED p12 //wall clock
+//Waiting seats
+
+//Define interrupt inputs
+DigitalIn  adult(BUTTON_1);
+DigitalIn  child(BUTTON_2);
+DigitalIn  fire_alarm(BUTTON_3);
+DigitalIn  temperature(SWITCH_2);
+
+//Define outputs
+DigitalOut no_entry(RED_LED);
+DigitalOut barber_1(BLUE_LED);
+DigitalOut barber_2(YELLOW_LED);
+DigitalOut barber_3(WHITE_LED);
+
+
