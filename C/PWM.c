2e71390cca6a703ab7e62ff0e13e9226ce4a99e2
MateoCortésD <57098389+MateoCorDuarte@users.noreply.github.com>
2025-07-04 11:08:36 +0200

    Update PWM.c

--- C/PWM.c
+++ C/PWM.c
@@ -9,19 +9,31 @@ to emit the signal of a speaker, that modify it's pitch and volume with two pote
 #define AIN2 p16
 
 // Define the PWM speaker output
+PwmOut speaker(SPEAKER);
+
 // Define analog inputs
+AnalogIn port1(AIN1);
+AnalogIn port2(AIN2);
 
 //Write your code here
 
 //Define variables
+float val1;
+float val2;
 float i;
 /*----------------------------------------------------------------------------
  MAIN function
  *----------------------------------------------------------------------------*/
 int main(){
     while(1){
+        val1 = port1.read();
+        val2 = port2.read();
         // Create a saw-tooth sound wave
         // Make the period and volume adjustable using the potentiometers
+        for(i=0;i<1;i+=0.05){
+            speaker.period(0.003125-(0.002*val1));
+            speaker = i*0.05*val2;
+        }
         wait_ms(100);
     }
 }
