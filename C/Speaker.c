c920345496cafe9ee52a31cc27db2c242a875d5a
MateoCortésD <57098389+MateoCorDuarte@users.noreply.github.com>
2025-07-19 16:42:38 +0200

    Create Speaker.c

---
+++ C/Speaker.c
@@ -0,0 +1,98 @@
+#include "mbed.h"
+
+#define SPEAKER p21
+#define RED_LED p22
+#define YELLOW_LED p23
+#define BLUE_LED p24
+#define AIN1 p15
+#define AIN2 p16
+
+// Define the frequency of basic music notes
+# define Do     0.5
+# define Re     0.45
+# define Mi     0.4
+# define Fa     0.36
+# define So     0.33
+# define La     0.31
+# define Si     0.3
+# define No     0 
+
+// Define the beat length (e.g. whole note, half note) 
+# define b1     0.5
+# define b2     0.25
+# define b3     0.125
+# define b4     0.075
+
+// Define the musical piece
+float note[] = {Mi,No,Mi,No,Mi,No, Mi,No,Mi,No,Mi,No, Mi,No,So,No,Do,No,Re,No,Mi,No, Fa,No,Fa,No,Fa,No,Fa,No, Fa,No,Mi,No,Mi,No,Mi,No,Mi,No, Mi,No,Mi,No,Re,No,Mi,No,Mi, Re,No,So,No};
+float beat[] = {b3,b3,b3,b3,b2,b2, b3,b3,b3,b3,b2,b2, b3,b3,b3,b3,b3,b3,b3,b3,b2,b1, b3,b3,b3,b3,b3,b3,b3,b3, b3,b3,b3,b3,b3,b3,b4,b4,b4,b4, b4,b4,b4,b4,b4,b4,b2,b4,b1, b1,b3,b2,b1};
+
+// Define the analog inputs
+// Write your code
+
+// Define the PWM output for the speaker
+// Write your code
+
+// Define the PWM output for the LED
+// Write your code
+
+// Define the time ticker
+// Write your code
+
+// Static variable
+static int k;
+static float volume;
+static float speed;
+
+
+/*----------------------------------------------------------------------------
+ Interrupt Service Routine
+ *----------------------------------------------------------------------------*/
+
+//Define the ticker ISR
+void timer_ISR(){
+
+	/*
+	The time ticker ISR will be periodically triggered after every single note
+		+ Update the PWM frequency to the next music note
+		+ Update beat length for the next music note (reconfigure the tick interrupt time)
+		+ Update the colour of the RGB LEDs to reflect the melody changing
+		+ The inputs from the two potentiometers will be used to adjust the volume and the speed
+	*/
+
+	if (k<(sizeof(note)/sizeof(int))){
+		
+		// If statement to test if the note array is currently indexed to a silent note
+		// Write your code here
+		
+		// Else statement that sets the speaker PWM period and duty cycle
+		// Write your code here
+
+
+		k++;
+			
+		// Set the time for the next ticker interrupt, this is determined by the default music beat and the potentiometer
+		// Write your code here
+			
+		// RGB LED indicator
+    	// Write your code here
+    
+	// Else statement to set k and the speaker to 0 to restart the melody
+	} else {
+		// Write your code here
+	}
+}
+
+int main() {
+
+	// Initialize the time ticker and set k to 0
+	// Write your code here
+
+    while (1) {
+
+        // Update the value of the volume and the speed 
+		// Write your code here
+
+		wait_ms(100);
+	}
+}
